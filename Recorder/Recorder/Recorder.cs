81b6aa93769c11787ec2111bbc99b4f494816936
Christopher Rose <rosc@tessella.com>
2014-04-25 13:42:21 +0200

    updated projects

---
+++ Recorder/Recorder/Recorder.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Recorder.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Recorder
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// Recorder class
+    /// </summary>
+    public class Recorder : IQueryable
+    {
+        /// <summary>
+        /// Implemented method from IQueryable to get value by key
+        /// </summary>
+        /// <param name="key">Key to search for</param>
+        /// <returns>String value or KeyNotFoundException</returns>
+        public string GetByKey(string key)
+        {
+            throw new KeyNotFoundException();
+        }
+    }
+}
---
+++ Recorder/RecorderTests/QueryTests.cs
@@ -0,0 +1,34 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="QueryTests.cs" company="Microsoft">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace RecorderTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using NUnit.Framework;
+    
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [TestFixture]
+    public class QueryTests
+    {
+        [Test]
+        public void Getting_key_from_empty_recorder_throws_exception()
+        {
+            // // Where
+            //var recorder = createStubRecorder();
+
+            //// When
+            //// Then
+
+            //Assert.Throws(typeof(KeyNotFoundException), delegate() { recorder.GetKeyByValue("asdf"); });
+        }
+    }
+}
